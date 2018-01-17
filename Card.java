c01be11649181aff0340bb55259a9c1ed7e99d68
Kevin Ford <ford.kevin@gmail.com>
2018-01-16 20:01:02 -0500

    Update Card.java
    Minor changes to logic.

--- Card.java
+++ Card.java
@@ -4,53 +4,21 @@
 /****************************************/
 public class Card {
 	
-	private char rank;
-	private char suit;
-	private int rankValue;
-	private int suitValue;
+	private char rank, suit;
+	private String suitRank;
 	
-	public Card(String rankSuit){
+	public Card(String suitRank){
 		
-		char[] a = rankSuit.toCharArray();
+		this.suitRank = suitRank;
+		char[] a = suitRank.toCharArray();
 		
-		rank = a[0];
-		suit = a[1];
-		setRankValue();
-		setSuitValue();
+		rank = a[1];
+		suit = a[0];
 	}
 	
 	public char getRank(){ return rank;}
 	
 	public char getSuit(){ return suit;}
 	
-	public int rankValue(){ return rankValue;}
-	
-	public int suitValue(){ return suitValue;}
-	
-	private void setRankValue(){
-		switch(rank){
-			case '2' : rankValue = 0;
-			case '3' : rankValue = 1;
-			case '4' : rankValue = 2;
-			case '5' : rankValue = 3;
-			case '6' : rankValue = 4;
-			case '7' : rankValue = 5;
-			case '8' : rankValue = 6;
-			case '9' : rankValue = 7;
-			case 'T' : rankValue = 8;
-			case 'J' : rankValue = 9;
-			case 'Q' : rankValue = 10;
-			case 'K' : rankValue = 11;
-			case 'A' : rankValue = 12;
-		}		
-	}
-	
-	private void setSuitValue(){
-		switch(rank){
-			case 'c' : suitValue = 0;
-			case 'd' : suitValue = 1;
-			case 'h' : suitValue = 2;
-			case 's' : suitValue = 3;			
-		}		
-	}
+	public String toString(){ return suitRank;}
 }
